eb9e71fcea07f4433104816299da068c1dc570db
pavel.tuzov <silent-sw@yandex.ru>
2018-11-27 14:50:01 +0500

    add modal

--- src/app/components/Tasks.jsx
+++ src/app/components/Tasks.jsx
@@ -1,5 +1,19 @@
 import * as React from 'react';
 import 'sass/app.scss';
+import Modal from "react-modal";
+
+const customStyles = {
+    content : {
+        top                   : '50%',
+        left                  : '50%',
+        right                 : 'auto',
+        bottom                : 'auto',
+        marginRight           : '-50%',
+        transform             : 'translate(-50%, -50%)'
+    }
+};
+Modal.setAppElement('#root');
+
 
 const API = 'http://localhost:8080/api/v1/';
 
@@ -14,17 +28,38 @@ class Tasks extends React.Component {
                 current_page: 1,
             },
             isLoading: false,
-            query: ''
+            query: '',
+            modalIsOpen: false,
+            isModalLoading: false,
+            modalData: ''
         };
 
         this.handleClickPaginate = this.handleClickPaginate.bind(this);
-        this.handleClickPopup = this.handleClickPopup.bind(this);
+
+        this.openModal = this.openModal.bind(this);
+        this.afterOpenModal = this.afterOpenModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
     }
 
-    handleClickPopup(event) {
+    openModal(event) {
+        this.setState({ isModalLoading: true });
         fetch(API + 'task/' + event.currentTarget.dataset.popup)
-            .then(response => response.json());
+            .then(response => response.json())
+            .then(data => this.setState({
+                modalData: data,
+                modalIsOpen: true,
+                isModalLoading: false
+            }));
     }
+
+    afterOpenModal() {
+        this.subtitle.style.color = '#f00';
+    }
+
+    closeModal() {
+        this.setState({modalIsOpen: false});
+    }
+
 
     handleClickPaginate(event) {
         this.setState({ isLoading: true });
@@ -96,6 +131,7 @@ class Tasks extends React.Component {
 
         return (
             <div>
+
                 <table className="table">
                     <thead>
                     <tr>
@@ -108,7 +144,7 @@ class Tasks extends React.Component {
                     {content.data.map(item =>
                         <tr
                             data-popup={item.id}
-                            onClick={this.handleClickPopup}
+                            onClick={this.openModal}
                             key={item.id}>
                           <th>{item.id}</th>
                           <td>{item.title}</td>
@@ -123,6 +159,53 @@ class Tasks extends React.Component {
                     {renderPageNumbers}
                     </ul>
                 </nav>
+                <Modal
+                    isOpen={this.state.modalIsOpen}
+                    onAfterOpen={this.afterOpenModal}
+                    onRequestClose={this.closeModal}
+                    style={customStyles}
+                    contentLabel="Example Modal"
+                >
+
+                    <h2 ref={subtitle => this.subtitle = subtitle}>Информация о задаче № {this.state.modalData.id}</h2>
+                    <div className="form-group row">
+                        <label htmlFor="inputTitle" className="col-sm-4 col-form-label">Заголовок</label>
+                        <div className="col-sm-8">
+                            <input type="text" className="form-control" id="inputTitle"
+                                placeholder="Заголовок" value={this.state.modalData.title}/>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label htmlFor="inputDate" className="col-sm-4 col-form-label">Дата выполнения</label>
+                        <div className="col-sm-8">
+                            <input type="text" className="form-control" id="inputDate"
+                                   placeholder="Дата выполнения" value={this.state.modalData.date}/>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label htmlFor="inputAuthor" className="col-sm-4 col-form-label">Автор</label>
+                        <div className="col-sm-8">
+                            <input type="text" className="form-control" id="inputAuthor"
+                                   placeholder="Автор" value={this.state.modalData.author}/>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label htmlFor="inputStatus" className="col-sm-4 col-form-label">Статус</label>
+                        <div className="col-sm-8">
+                            <input type="text" className="form-control" id="inputStatus"
+                                   placeholder="Статус" value={this.state.modalData.status}/>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label htmlFor="inputDesc" className="col-sm-4 col-form-label">Описание</label>
+                        <div className="col-sm-8">
+                            <textarea className="form-control" id="inputDesc"
+                                      placeholder="Описание">{this.state.modalData.description}</textarea>
+                        </div>
+                    </div>
+
+                    <button class="btn-block" onClick={this.closeModal}>Закрыть</button>
+                </Modal>
             </div>
         );
     }
